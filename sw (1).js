a9f6de71395680b3704a341a098da30e968df2fd
mateusbrito2005-design <mateusbrito2005@gmail.com>
2026-08-30 22:44:26 -0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'missao-sem-telas-v1';
+const ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResponse.clone()));
+        return networkResponse;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
